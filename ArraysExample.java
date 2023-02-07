12dcf57e6f9625255409669249873173118b8db8
Midokhalil1 <midokhalil22@gmail.com>
2023-02-07 14:41:19 -0500

    using add to the list

--- ArraysExample.java
+++ ArraysExample.java
@@ -2,12 +2,13 @@ public class ArraysExample {
     public static void main(String[] args) {
         int[] myNumbers = {10, 20, 30, 40, 50, 60, 70} ;
 
-     
+        myNumbers[0] = 145;
+        myNumbers[5] = 120;
 
      
         
         for(int index = 0; index < myNumbers.length; index++){
-            System.out.println(myNumbers[index]);
+            System.out.print(myNumbers[index] + " ");
         }
     }
 }
