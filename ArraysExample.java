a7a91e458144e51407e2be00decf7daf86e162dd
Midokhalil1 <midokhalil22@gmail.com>
2023-02-07 14:07:19 -0500

    updates

---
+++ ArraysExample.java
@@ -0,0 +1,13 @@
+public class ArraysExample {
+    public static void main(String[] args) {
+        int[] myNumbers = {10, 20, 30, 40, 50, 60, 70} ;
+
+     
+
+     
+        
+        for(int index = 0; index < myNumbers.length; index++){
+            System.out.println(myNumbers[index]);
+        }
+    }
+}
